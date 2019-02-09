cbefe5503de3bcae1e8c96a8a0f3aea90da99c9d
Aaron Quinton <aaron.wquinton@gmail.com>
2019-02-09 15:08:06 -0800

    updated unit test functions

--- tests/testthat/test_lib_writeme.R
+++ tests/testthat/test_lib_writeme.R
@@ -1,1 +1,13 @@
-# Test for lib_writeme() function
+# input: a script
+input <- "tests/scripts/summarized_data.R"
+
+# expected output: a text file documenting the dependancies
+exp_output <- read_file("tests/sample.txt")
+
+
+test_that("The Text file generated is as expected with simple case", {
+
+  expect_equal(read_file(lib_writeme("tests/scripts/summarized_data.R")), exp_output)
+
+})
+
