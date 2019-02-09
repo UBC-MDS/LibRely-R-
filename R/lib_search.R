538acb8b30454fef507981dc429c71c86bf11fa6
Zixin Zhang <zxzzhangg@gmail.com>
2019-02-09 00:52:28 -0800

    add search function

--- R/lib_search.R
+++ R/lib_search.R
@@ -1,5 +1,7 @@
+#  Feb 8th, 2019
+#  Zixin Zhang
 #' Find dependant packages
-#'
+#' 
 #' Input a R script to find all the dependant packages used and create a character vector with all of their names.
 #'
 #' @param script a .R file
@@ -11,6 +13,19 @@
 #' lib_search("script.R")
 lib_search <- function(script){
 
-
-  return("test")
+  ##' This function takes an input R script and return the libraries that used in this script  
+  ##'
+  ##' Parameters
+  ##' ---------
+  ##' input_path(string): the path to the input .R script
+  ##' 
+  ##' Return
+  ##' --------
+  ##' depen(vector): a vector that stores all the dependencies
+  
+  
+  return(depen)
 }
+
+
+
