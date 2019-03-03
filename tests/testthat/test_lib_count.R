f217996047fd1e74189995c5f5c89e76c6e7fa2b
Zixin Zhang <zxzzhangg@gmail.com>
2019-03-02 18:58:28 -0800

    update the test

--- tests/testthat/test_lib_count.R
+++ tests/testthat/test_lib_count.R
@@ -1,14 +1,40 @@
+context("Test lib_count")
+
 # input: a script
-input <- "tests/scripts/summarized_data.R"
+input1 <- "file/script_for_test1.R"
+input3 <- "file/script_for_test3.R"
 
 # expected output: a dataframe of the package, function and count
-exp_output <- data.frame(pkg = c("dplyr", "dplyr"),
-                         functions = c("mutate", "summarize"),
-                         count = c(1,1))
+library(tidyverse)
+exp_output1 <- tibble(package_name = c("dplyr", "dplyr","dplyr","tibble"),
+                          function_name = c("mutate", "summarize_all","tibble","tibble"),
+                          count = as.integer(c(1,1,1,1)))
 
 
-test_that("Simple case with one pkg and two funs", {
+test_that("If the function has correct output", {
 
-  expect_equal(lib_count("tests/scripts/summarized_data.R"), exp_output)
+  expect_equal(lib_count(input1), exp_output1)
+  expect_equal(lib_count(input3), NULL)
 
+})
+
+test_that("In case the input is not a .R script", {
+
+  expect_error(lib_count("test.txt"))
+  expect_error(lib_count("test.pdf"))
+
+})
+
+test_that("If user specifies an additional argument, it throws an error", {
+
+  expect_error(lib_count(input, "script"))
+
+})
+
+test_that("In case the input is not a path", {
+
+  expect_error(lib_count(123))
+  expect_error(lib_count(c(1,2,3)))
+  expect_error(lib_count("This is a string"))
+  expect_error(lib_count(data.frame(a=c(1,2),b=c("one","second"))))
 })
